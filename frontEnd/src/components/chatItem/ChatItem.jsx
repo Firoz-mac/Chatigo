133232b2b6648e10b8dee427b64f8f8cc635c031
Firoz <firozmhd23@gmail.com>
2026-02-10 02:00:48 +0530

    Enhance ChatItem and Home components: Add onClick handler to ChatItem, manage chat state in Home, and improve UI for unselected chat state

--- frontEnd/src/components/chatItem/ChatItem.jsx
+++ frontEnd/src/components/chatItem/ChatItem.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './chatItem.css'
 
-const ChatItem = () => {
+const ChatItem = ({onClick}) => {
   return (
-    <div className='chatItem'>
+    <div className='chatItem' onClick={onClick}>
         <div className="profileWrapper">
             <img src="https://plus.unsplash.com/premium_photo-1690407617686-d449aa2aad3c?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGZlbWFsZSUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D" alt="" />
         </div>
--- frontEnd/src/pages/home/Home.jsx
+++ frontEnd/src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './home.css'
 import logo from '../../assets/Logo/ChatigoLogoWithNameDark.png'
 import { IoIosAddCircle } from "react-icons/io";
@@ -10,19 +10,23 @@ import { BsEmojiSmile } from "react-icons/bs";
 import { IoMdSend } from "react-icons/io";
 import { IoIosAttach } from "react-icons/io";
 import { MdKeyboardVoice } from "react-icons/md";
+import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
 import ChatItem from '../../components/chatItem/ChatItem';
 import Message from '../../components/message/Message';
 
 
 const Home = () => {
+    const [chatOpen, setChatOpen] = useState(false);
     const bottomRef=useRef(null);
 
-    useEffect(()=>{
-        bottomRef.current?.scrollIntoView({behavior:'smooth'})
-    },[])
+    useEffect(() => {
+        if (chatOpen) {
+            bottomRef.current?.scrollIntoView({ behavior: 'smooth' })
+        }
+    }, [chatOpen])
 
   return (
-    <div className='home'>
+    <div className={`home ${chatOpen ? 'chatOpen' : ''}`}>
         <div className="chatList">
             <div className="chatListHead">
                 <div className="logoSec">
@@ -35,7 +39,7 @@ const Home = () => {
                 </div>
             </div>
             <div className="chatListBody">
-                <ChatItem/>
+                <ChatItem onClick={() => setChatOpen(true)}/>
                 <ChatItem/>
                 <ChatItem/>
                 <ChatItem/>
@@ -56,44 +60,52 @@ const Home = () => {
             </div>
         </div>
         <div className="chat">
-            <div className="chatHead">
-                <div className="chatHeadProfileSec">
-                    <div className="profileWrapper">
-                        <img src="https://plus.unsplash.com/premium_photo-1690407617686-d449aa2aad3c?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGZlbWFsZSUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D" alt="" />
+            {chatOpen ?
+            <>
+                <div className="chatHead">
+                    <div className="chatHeadProfileSec">
+                        <MdOutlineKeyboardArrowLeft className='backArrowIcon' onClick={() => setChatOpen(false)} />
+                        <div className="profileWrapper">
+                            <img src="https://plus.unsplash.com/premium_photo-1690407617686-d449aa2aad3c?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGZlbWFsZSUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D" alt="" />
+                        </div>
+                        <div className="profileInfo">
+                            <span className='chatName'>Mac</span>
+                            <span className='status'>Online</span>
+                        </div>
                     </div>
-                    <div className="profileInfo">
-                        <span className='chatName'>Mac</span>
-                        <span className='status'>Online</span>
+                    <div className="chatHeadIcons">
+                        <IoIosCall  className='icon'/>
+                        <TbVideoFilled className='icon'/>
+                        <BiDotsVerticalRounded className='icon'/>
                     </div>
                 </div>
-                <div className="chatHeadIcons">
-                    <IoIosCall  className='icon'/>
-                    <TbVideoFilled className='icon'/>
-                    <BiDotsVerticalRounded className='icon'/>
+                <div className="chatContent">
+                    <Message sender="me"/>
+                    <Message sender="other"/>
+                    <Message sender="me"/>
+                    <Message sender="me"/>
+                    <Message sender="other"/>
+                    <Message sender="other"/>
+                    <Message sender="other"/>
+                    <Message sender="me"/>
+                    <Message sender="me"/>
+                    <Message sender="me"/>
+                    <div ref={bottomRef} />
                 </div>
-            </div>
-            <div className="chatContent">
-                <Message sender="me"/>
-                <Message sender="other"/>
-                <Message sender="me"/>
-                <Message sender="me"/>
-                <Message sender="other"/>
-                <Message sender="other"/>
-                <Message sender="other"/>
-                <Message sender="me"/>
-                <Message sender="me"/>
-                <Message sender="me"/>
-                <div ref={bottomRef} />
-            </div>
-            <div className="chatInputSec">
-                <BsEmojiSmile className='emojiIcon'/>
-                <input type="text" placeholder='Type a message'/>
-                <IoIosAttach  className='attachIcon'/>
-                <div className="btn">
-                    {/* <IoMdSend /> */}
-                    <MdKeyboardVoice />
+                <div className="chatInputSec">
+                    <BsEmojiSmile className='emojiIcon'/>
+                    <input type="text" placeholder='Type a message'/>
+                    <IoIosAttach  className='attachIcon'/>
+                    <div className="btn">
+                        {/* <IoMdSend /> */}
+                        <MdKeyboardVoice />
+                    </div>
                 </div>
-            </div>
+            </>
+            :   <div className="chatUnselected">
+                    <span>Select a Chat to Start Messaging</span>
+                </div> 
+            }
         </div>
     </div>
   )
